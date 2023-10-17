c6d45e42fe407c77a0b064ffd856ee05cfcb2617
Dominik Picheta <dominik@cloudflare.com>
2023-10-17 22:08:14 +0100

    Draw some text.

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ export default {
         // A simple example of how to run code directly. In this case using
         // Pillow to render an image and return it from the worker.
         const pyodide = await getPyodide();
+
         pyodide.runPython(`
-          from PIL import Image
-          img = Image.new(mode="RGB", size=(800, 600), color=(231, 136, 59))
-          img.save("/result.png")
+from PIL import Image, ImageFont, ImageDraw
+img = Image.new(mode="RGB", size=(200, 50), color=(231, 136, 59))
+# get a font
+fnt = ImageFont.load_default();
+# get a drawing context
+d = ImageDraw.Draw(img)
+
+# draw multiline text
+d.multiline_text((10, 10), "Hello World", font=fnt, fill=(0, 0, 0))
+img = img.resize((400, 100))
+img.save("/result.png")
         `);
 
         let file = pyodide.FS.readFile("/result.png", { encoding: "binary" });
